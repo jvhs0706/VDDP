bab98417f69b2c79ede0de61f7c457a15532ab9b
Haochen Sun <haochen.sun@uwaterloo.ca>
2024-11-27 04:11:02 +0000

    ...?

--- src/vddlm.hpp
+++ src/vddlm.hpp
@@ -10,10 +10,16 @@ using namespace std;
 
 vector<bool> probToBits(double p, uint n);
 
+void readVDDLMConfig(const string& config_file, vector<bool>& z_config, vector<vector<bool>>& g_config);
+
 vector<bool> Bernoulli(vector<bool> p, const LegendrePRNGPubParam& pp, Timer& comp_timer, Timer& ptimer, Timer& vtimer, Polynomial& F_out, Polynomial& R_out, G1& com_out);
 
 vector<uint> Geometric(double p, uint log_range, uint prec, const LegendrePRNGPubParam& pp, Timer& comp_timer, Timer& ptimer, Timer& vtimer, Polynomial& F_out, Polynomial& R_out, G1& com_out);
 
 vector<int> DiscreteLaplacian(double t, uint log_range, uint prec, const LegendrePRNGPubParam& pp, Timer& comp_timer, Timer& ptimer, Timer& vtimer, Polynomial& F_out, Polynomial& R_out, G1& com_out);
+
+vector<uint> GeometricNew(const vector<vector<bool>>& config, const LegendrePRNGPubParam& pp, Timer& comp_timer, Timer& ptimer, Timer& vtimer, Polynomial& F_out, Polynomial& R_out, G1& com_out);
+
+vector<int> DiscreteLaplacianNew(const vector<bool>& z_config, const vector<vector<bool>>& g_config, const LegendrePRNGPubParam& pp, Timer& comp_timer, Timer& ptimer, Timer& vtimer, Polynomial& F_out, Polynomial& R_out, G1& com_out);
 
 #endif // VDDLM_HPP
